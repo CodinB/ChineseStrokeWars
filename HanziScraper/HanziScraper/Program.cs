12662a64dbf25d7dcf1f799631195ac298c9f7a3
Briun Greene <briun.greene@gmail.com>
2017-11-08 10:25:25 -0800

    wip

--- HanziScraper/HanziScraper/Program.cs
+++ HanziScraper/HanziScraper/Program.cs
@@ -6,36 +6,15 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-
-namespace webscrapper
+namespace HanziScraper
 {
     class Program
     {
-        //static void Main(string[] args)
-        //{
-        //    // Setup the configuration to support document loading
-        //    var config = Configuration.Default.WithDefaultLoader();
-        //    // Load the names of all The Big Bang Theory episodes from Wikipedia
-        //    var address = "https://en.wikipedia.org/wiki/List_of_The_Big_Bang_Theory_episodes";
-        //    // Asynchronously get the document in a new context using the configuration
-        //    var document = BrowsingContext.New(config).OpenAsync(address).Result;
-        //    // This CSS selector gets the desired content
-        //    var cellSelector = "tr.vevent td:nth-child(3)";
-        //    // Perform the query to get all cells with the content
-        //    var cells = document.QuerySelectorAll(cellSelector);
-        //    // We are only interested in the text - select it with LINQ
-        //    var titles = cells.Select(m => m.TextContent);
-        //    foreach (var title in titles)
-        //    {
-        //        Console.WriteLine(title);
-        //    }
-        //}
-
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             var config = Configuration.Default.WithDefaultLoader();
-            var address = "http://www.hanzicraft.com/character/意";
+            var address = "http://www.hanzicraft.com/character/藏";
             //var address = "view-source:http://hskhsk.pythonanywhere.com/radicals?hsk=16";
             var document = BrowsingContext.New(config).OpenAsync(address).Result;
             var cellSelector = "div.decomptitle";
@@ -75,9 +54,7 @@ namespace webscrapper
             Console.WriteLine(outputString);
 
 
+
         }
-
-
     }
-
 }
