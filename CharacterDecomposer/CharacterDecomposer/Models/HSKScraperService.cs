b8f9fed93d746abf11f0b235d1b5a3114ed86639
Briun Greene <briun.greene@gmail.com>
2017-12-19 18:12:33 -0800

    wip

---
+++ CharacterDecomposer/CharacterDecomposer/Models/HSKScraperService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace CharacterDecomposer.Models
+{
+    public class HSKScraperService
+    {
+        public List<HSKWords> GetHskWords()
+        {
+            using (SqlConnection con = new SqlConnection("data source =.;database=HSK1-6_Words; integrated security=SSPI"))
+            {
+                con.Open();
+
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandText = "Words_Get_Random";
+
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    List<HSKWords> results = new List<HSKWords>();
+                    while (dr.Read())
+                    {
+                        HSKWords word = new HSKWords();
+                        word.Id = dr.GetInt32(0);
+                        word.Word = dr.GetString(1);
+                        word.Hsklevel = dr.GetInt32(2);
+
+                        results.Add(word);
+                    }
+                    return results;
+                }
+            }
+        }
+    }
+}
---
+++ CharacterDecomposer/CharacterDecomposer/Models/HSKWords.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CharacterDecomposer.Models
+{
+    public class HSKWords
+    {
+        public int Id { get; set; }
+        public string Word { get; set; }
+        public int Hsklevel { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Hsklevel}: {Word}";
+        }
+    }
+}
